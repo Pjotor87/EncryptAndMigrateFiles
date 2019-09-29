eb4756439aef3b1c00a54d141ccb63fbc8274ab4
Peter Andersson <pjotor87@hotmail.com>
2019-09-29 09:12:24 +0200

    Added conditionals for processes to run in send.ps1 and receive.ps1

--- Receive.ps1
+++ Receive.ps1
@@ -1,5 +1,5 @@
 [xml]$config = Get-Content -Path ($PSScriptRoot + "\" + "Config.xml")
-Invoke-Expression "& `"$PSScriptRoot\_030_Receive_Copy.ps1`""
+if($config.Root.Receive.CopyFromSourceToTargetFolder -eq "True"){ Invoke-Expression "& `"$PSScriptRoot\_030_Receive_Copy.ps1`"" }
 if($config.Root.Receive.DeleteWhenFinished -eq "True"){ Invoke-Expression "& `"$PSScriptRoot\_035_Receive_DeleteCopy.ps1`"" }
-Invoke-Expression "& `"$PSScriptRoot\_040_Receive_Unzip.ps1`""
+if($config.Root.Receive.UnzipToTargetFolder -eq "True"){ Invoke-Expression "& `"$PSScriptRoot\_040_Receive_Unzip.ps1`"" }
 if($config.Root.Receive.DeleteWhenFinished -eq "True"){ Invoke-Expression "& `"$PSScriptRoot\_045_Receive_DeleteUnzip.ps1`"" }
